f03c9f74a7649e7d69b1d74778f677d8a3946a74
Anna D Derbakova <hiopa16@gmail.com>
2017-02-14 00:49:39 -0500

    Adding firebase configuration files
    Adding the config files that are required to deploy and host on
    Firebase.

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDzKRAb5-uPyTF3L8k5Mu5OC3SwkIoBTuI",
+	authDomain: "multiplayer-rps-9d62f.firebaseapp.com",
+	databaseURL: "https://multiplayer-rps-9d62f.firebaseio.com",
+	storageBucket: "multiplayer-rps-9d62f.appspot.com",
+	messagingSenderId: "146788664111"
+};
+firebase.initializeApp(config);
