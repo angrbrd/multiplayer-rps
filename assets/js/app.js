33e939b9d5dad9ae1f2881201e226659ed811faa
Anna D Derbakova <hiopa16@gmail.com>
2017-02-15 01:19:34 -0500

    Adding Firebase add user functionality
    Added Firebase event handling for listening for database modifications.
    Also added event handling for the “Submit” button to add users to the
    game. Users can now join by clicking the “Submit” button and the
    database is updated accordingly.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,96 @@
-// Placeholder JavaScript
-$(document).ready(
-	console.log("XXX ENTER JavaScript XXX")
-);
+/*
+//
+//  Global Variables
+//
+*/
+
+var player1 = null;
+var player2 = null;
+
+/*
+//
+//  Firebase Database Section
+//
+*/
+
+// Get a reference to the database service
+var database = firebase.database();
+
+// Attach a listener to the database /players/ node to listen for any changes
+database.ref('/players/').on('value', function(snapshot) {
+	// Check for existence of player 1 in the database
+	if (snapshot.child("player1").exists()) {
+		console.log("Player 1 exists");
+
+		// Record player1 data
+		player1 = snapshot.val().player1;
+		console.log(JSON.stringify(player1));
+
+		// Update player1 display
+		$("#playerOneName").text(player1.name);
+	} else {
+		console.log("Player 1 does NOT exist");
+
+		player1 = null;
+
+		// Update player1 display
+		$("#playerOneName").text("Waiting for Player 1...");
+	}
+
+	// Check for existence of player 2 in the database
+	if (snapshot.child("player2").exists()) {
+		console.log("Player 2 exists");
+
+		// Record player2 data
+		player2 = snapshot.val().player2;
+		console.log(JSON.stringify(player2));
+
+		// Update player2 display
+		$("#playerTwoName").text(player2.name);
+	} else {
+		console.log("Player 2 does NOT exist");
+
+		player2 = null;
+
+		// Update player2 display
+		$("#playerTwoName").text("Waiting for Player 2...");
+	}
+});
+
+// Attach an event handler to the "Submit" button to add a new user to the database
+$("#add-name").on("click", function(event) {
+	event.preventDefault();
+
+	// Adding player1
+	if (player1 === null) {
+		console.log("Adding Player 1");
+
+		player1 = {
+			name: $("#name-input").val().trim(),
+			win: 0,
+			loss: 0,
+			tie: 0,
+			choice: ""
+		};
+
+		// Add player1 to the database
+		database.ref().child('/players/player1').set(player1);
+	} else if( (player1 !== null) && (player2 === null) ) {
+		// Adding player2
+		console.log("Adding Player 2");
+
+		player2 = {
+			name: $("#name-input").val().trim(),
+			win: 0,
+			loss: 0,
+			tie: 0,
+			choice: ""
+		};
+
+		// Add player2 to the database
+		database.ref().child('/players/player2').set(player2);
+	}
+
+	// Reset the name input box
+	$("#name-input").val("");	
+});
